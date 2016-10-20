577d6050361fa825e0534803c59d5d936805a170
AnaasN <nice-anaas@hotmail.co.uk>
2016-10-20 12:02:59 +0100

    Update 1

--- ForeSurNameSelection/Program.cs
+++ ForeSurNameSelection/Program.cs
@@ -23,8 +23,8 @@ namespace ForeSurNameSelection
                 LenghtOfUserFullName = UserFullName.Length;//use of a method
                 PositionOfComa = UserFullName.IndexOf(',');// finds the position of ','
                 UserSurname = UserFullName.Substring(0, PositionOfComa);//makes the surname equal to the characters from index 0 to where the coma is.
-                UserForename = UserFullName.Substring(PositionOfComa, LenghtOfUserFullName - PositionOfComa);//makes forename the charcters from where the coma is to the end of the strin.
-                UserForename = UserForename.Replace(',', ' ');//ask sir, to find a better alternative.
+                UserForename = UserFullName.Substring(Position of coma + 1);//makes forename the charcters from where the coma is to the end of the strin.
+               // UserForename = UserForename.Replace(',', ' ');//ask sir, to find a better alternative.
                 Console.WriteLine("Firstname:{0}", UserForename);
                 Console.WriteLine("Surname: {0}.", UserSurname);
             }
